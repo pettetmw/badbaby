ef65ebc3711a585d5e3540604f0c417c7e272ea7
Kambiz Tavabi <ktavabi@uw.edu>
2018-09-25 15:01:12 -0700

    Consolidate & Update
    Consolidated data reading routines updated spreadsheets.

--- badbaby/return_dataframes.py
+++ badbaby/return_dataframes.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Script returns cohort specific pandas dataframes """
+"""Helpers to return cohort specific dataframes """
 
 # Authors: Kambiz Tavabi <ktavabi@gmail.com>
 # License: MIT
@@ -13,57 +13,49 @@ from badbaby import parameters as params
 static_dir = params.static_dir
 
 
-def return_simms_mmn_dfs():
+def return_dataframes(paradigm, age=None, bezos=False, simms=False):
     """
-       Return MMN exam and CDI dataframes for Simms-Mann cohort
-       Returns
-       -------
-       tuple
-           dataframe of selected cases from subject sample with
-           completed MMN MEG data and dataframe of corresponding CDI data.
-       """
-    # Read excel sheets into pandas dataframes
-    xl_a = pd.read_excel(op.join(static_dir, 'badbaby.xlsx'), sheet_name='MMN',
-                         converters={'BAD': str})
-    xl_b = pd.read_excel(op.join(static_dir, 'cdi_report_July_2018.xlsx'),
-                         sheet_name='WS')
-    # Exclude subjects
-    df = xl_a[xl_a['simms_inclusion'] == 1]
-    df = df.drop('Notes', axis=1)
-    df.dropna()
-    subjects = df['Subject_ID'].values.tolist()
-    subjects = np.unique(np.asarray(['BAD_%s' % ss[:3] for ss in subjects]))
-    dfs = list()
-    ages = np.unique(xl_b.CDIAge.values)
-    for age in ages:
-        _, _, mask = np.intersect1d(subjects,
-                                    xl_b[xl_b.CDIAge == age]
-                                    ['ParticipantId'].values,
-                                    return_indices=True)
-        dfs.append(xl_b[xl_b.CDIAge == age].iloc[mask])
-    cdi_df = pd.concat(dfs, ignore_index=True)
-    return df, cdi_df
+    Return available MEG and corresponding CDI datasets.
+    Parameters
+    ----------
+    paradigm:str
+    Name of project paradigm. Can be mmn, tone, or ids.
+    age:int
+    If not None (default) filter cohort data based on age.
+    simms:bool
+    Default False. If True then include only Simms-Mann longitudinal cohort.
+    bezos:bool
+    Default False. If True then include only Bezos SES cohort.
 
-
-def return_ford_mmn_dfs():
-    """
-    Return MMN exam and CDI dataframes for Ford cohort
     Returns
     -------
     tuple
-        dataframe of selected cases from subject sample with
-        completed MMN MEG data and dataframe of corresponding CDI data.
+        Pandas dataframes of available MEG and corresponding CDI datasets.
     """
     # Read excel sheets into pandas dataframes
-    xl_a = pd.read_excel(op.join(static_dir, 'badbaby.xlsx'), sheet_name='MMN',
+    xl_a = pd.read_excel(op.join(static_dir, 'badbaby.xlsx'),
+                         sheet_name=paradigm,
                          converters={'BAD': str})
-    xl_a = xl_a[(xl_a.complete == 1) & (xl_a.CDI == 1) & (xl_a.SES > 0)]
-    xl_a = xl_a.drop('Notes', axis=1, inplace=False)
-    xl_a.dropna()
+    xl_a = xl_a[(xl_a.complete == 1) & (xl_a.CDI == 1)]
+    # Subselect by cohort
+    if bezos:
+        xl_a = xl_a[xl_a['SES'] > 0]
+    elif simms:
+        xl_a = xl_a[xl_a['simms_inclusion'] == 1]
+    else:
+        if age is not None:
+            raise ValueError
+        xl_a = xl_a.drop('Notes', axis=1, inplace=False)
+        xl_a.dropna()
+        #  Filter by age
+        if age == 2:
+            xl_a = xl_a[xl_a['Age(days)'] < 70]
+        elif age == 6:
+            xl_a = xl_a[xl_a['Age(days)'] > 70]
     df = xl_a
     subject_ids = df.Subject_ID.values
     subject_ids = set(['BAD_%s' % ss[:3] for ss in subject_ids.tolist()])
-    xl_b = pd.read_excel(op.join(static_dir, 'cdi_report_July_2018.xlsx'),
+    xl_b = pd.read_excel(op.join(static_dir, 'cdi_report_final_08292018.xlsx'),
                          sheet_name='WS')
     participant_ids = np.unique(xl_b.ParticipantId.values)
     # Exclude subjects without CDI data
@@ -83,20 +75,3 @@ def return_ford_mmn_dfs():
         dfs.append(xl_b[xl_b.CDIAge == age].iloc[mask])
     cdi_df = pd.concat(dfs, ignore_index=True)
     return df, cdi_df
-
-
-def return_mmn_df():
-    """
-    Return MMN exam dataframe
-    Returns
-    -------
-    dataframe
-        dataframe of selected cases from subject sample with
-        completed MMN MEG data.
-    """
-    # Read excel sheets into pandas dataframes
-    xl_a = pd.read_excel(op.join(static_dir, 'badbaby.xlsx'), sheet_name='MMN',
-                         converters={'BAD': str})
-    xl_a.drop('Notes', axis=1, inplace=True).dropna()
-    # Exclude subjects
-    return xl_a[xl_a.complete == 1]
