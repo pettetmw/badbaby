e19058a16617e8feb56b8f11a6c4824ed18b9669
Kambiz Tavabi <ktavabi@gmail.com>
2018-12-18 12:05:10 -0800

    WIP Sphericity adjusted F-tests with eZANOVA

--- badbaby/CDI_rmANOVA.R
+++ badbaby/CDI_rmANOVA.R
@@ -22,6 +22,8 @@ if(!require(multcompView)){install.packages("multcompView")}
 if(!require(lsmeans)){install.packages("lsmeans")}
 if(!require(ggplot2)){install.packages("ggplot2")}
 if(!require(rcompanion)){install.packages("rcompanion")}
+if(!require(rcompanion)){install.packages("apa")}
+if(!require(rcompanion)){install.packages("ez")}
 
 # Imports
 library(psych)
@@ -32,7 +34,10 @@ library(multcompView)
 library(emmeans)
 library(rcompanion)
 library(ggplot2)
+library(apa)
+library(ezANOVA)
 
+setwd("~/Projects/Work/badbaby/badbaby/static")
 CDIdf <- read.csv(file = "CDIdf_RM.csv",
                   header=TRUE, sep="\t")
 CDIdf$CDIAge <- as.factor(CDIdf$CDIAge)
@@ -72,42 +77,47 @@ fmla <- as.formula("M3L ~ CDIAge + ses_group + Sex +
                     ses_group * Sex + CDIAge *
                     ses_group * Sex")
 
-# Mixed effect model
-model.mixed = lme(fmla, random=~ 1 | Subject_ID,
-correlation=corAR1(form=~ 1 | Subject_ID), data=CDIdf, method="REML")
 
-summary(model.mixed)
-Anova(model.mixed)
+##############
+# Null model #
+##############
+model.null = lme(M3L ~ 1, random=~1|Subject_ID, data=CDIdf)
+summary(model.null)
+
 
 ######################
 # Fixed effect model #
 ######################
-model.fixed = gls(fmla, correlation=corAR1(form=~1|Subject_ID),
-                  data=CDIdf, method='REML')
-
+model.fixed = gls(fmla, data=CDIdf, method='REML')
 summary(model.fixed)
-Anova(model.fixed)
 
-##############################
-# Model comparison/selection #
-##############################
+######################
+# Mixed effect model #
+######################
 # The random effects in the model can be tested by comparing the model to a
 # model fitted with just the fixed effects and excluding the random effects.
 # Because there are not random effects in this second model, the gls function
 # in the nlme package is used to fit this model.
+model.mixed = lme(fmla, random=~ 1 | Subject_ID, data=CDIdf, method="REML")
+summary(model.mixed)
 
-anova(model.mixed, model.fixed) # compares the reduction in the residual sum of squares
-# NB the residual sum of squares (RSS), also known as the sum of squared
+##############################
+# Model comparison/selection #
+##############################
+# Compare the reduction in the residual sum of squares. The residual sum of 
+# squares (RSS), also known as the sum of squared
 # residuals (SSR) or the sum of squared errors of prediction (SSE), is the sum
 # of the squares of residuals (deviations predicted from actual empirical
 # values of data). It is a measure of the discrepancy between the data and an
 # estimation model. A small RSS indicates a tight fit of the model to the data.
 # It is used as an optimality criterion in parameter selection and model
 # selection.
+anova(model.mixed, model.fixed) 
 
-###########################################################
-# Pseudo R^2 Measures of Fit for fixed model of latencies #
-###########################################################
+
+#######################
+# Pseudo R^2 Measures #
+#######################
 # The nagelkerke function can be used to calculate a p-value and pseudo
 # R-squared value for the model. One approach is to define the null model as
 # one with no fixed effects except for an intercept, indicated with a 1 on
@@ -123,37 +133,49 @@ anova(model.mixed, model.fixed) # compares the reduction in the residual sum of 
 # outcome.  In this situation, the higher pseudo R-squared indicates which
 # model better predicts the outcome.
 # https://stats.idre.ucla.edu/other/mult-pkg/faq/general/faq-what-are-pseudo-r-squareds/
-
-model.null = lme(M3L ~ 1, random=~1|Subject_ID, data=CDIdf)
-nagelkerke(model.mixed, model.null)
 nagelkerke(model.fixed, model.null)
+
 
 #####################
 # Post-Hoc analysis #
 #####################
+Anova(model.fixed)
 marginal = emmeans(model.fixed, ~ CDIAge : ses_group)
 cld(marginal, alpha=0.05, Letters=letters,  ### Use lower-case letters for .group
     adjust="tukey",  ###  Tukey-adjusted comparisons
     details=TRUE)
 
 Sum = groupwiseMean(M3L ~ CDIAge + ses_group,
-                    data=CDIdf, conf=0.95, digits=3, traditional=FALSE, 
+                    data=CDIdf, conf=0.95, digits=3, boot = TRUE, bca = TRUE,
+                    traditional = FALSE,
                     percentile=TRUE)
+print(Sum)
 
 pd = position_dodge(.2)
-ggplot(Sum, aes(x=CDIAge, y=Mean, color=ses_group)) +
-    geom_errorbar(aes(ymin=Percentile.lower,
-        ymax=Percentile.upper),
+ggplot(Sum, aes(x=CDIAge, y=Boot.mean, color=ses_group)) +
+    geom_errorbar(aes(ymin=Bca.lower,
+        ymax=Bca.upper),
         width=.2, size=0.7, position=pd) +
     geom_point(shape=20, size=4, position=pd) +
     theme_bw() +
     theme(axis.title=element_text(face="bold")) +
     ylab("M3L")
 
+
+
 # APA
+ezDesign(data = CDIdf[CDIdf$CDIAge==30,],
+         x = M3L,
+         y = ParticipantId,
+         col = ses_group)
+m3l_anova = ezANOVA(data = CDIdf[CDIdf$CDIAge==30,], 
+                    dv = M3L, 
+                    wid = ParticipantId, 
+                    within_full = Sex,
+                    between = ses_group,
+                    within_covariates = .(HC, SES, Age.days.))
+print(m3l_anova)
 aov.fit <- aov(fmla, data=CDIdf)
 summary(aov.fit)
 predict(aov.fit, CDIdf)
-anova_apa(aov.fit, sph_corr = "gg", es = "petasq", info = TRUE, format = "latex")
-apa.aov.table(model.mixed, filename, table.number = NA, conf.level = 0.9,
-              type = 3)
+anova_apa(aov.fit, sph_corr = "gg", es = "petasq", info = TRUE, format = "markdown")
