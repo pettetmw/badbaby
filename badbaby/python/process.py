5fec6f0f6836b99180e45bf8ddc65a6b861377aa
Kambiz Tavabi <ktavabi@gmail.com>
2019-06-05 14:21:27 -0700

    Revised MNEFUN script to use MF autobad selection and other tweaks

--- badbaby/python/process.py
+++ badbaby/python/process.py
@@ -2,67 +2,72 @@
 
 """Infant auditory MEG data MNEFUN processing pipeline."""
 
-__author__ = "Kambiz Tavabi"
-__copyright__ = "Copyright 2018, Seattle, Washington"
-__license__ = "MIT"
-__version__ = "0.1.0"
-__maintainer__ = "Kambiz Tavabi"
-__email__ = "ktavabi@uw.edu"
-__status__ = "Development"
+__author__ = 'Kambiz Tavabi'
+__copyright__ = 'Copyright 2018, Seattle, Washington'
+__license__ = 'MIT'
+__version__ = '0.1.0'
+__maintainer__ = 'Kambiz Tavabi'
+__email__ = 'ktavabi@uw.edu'
+__status__ = 'Development'
 
 import os.path as op
-import numpy as np
+
 import mnefun
+import numpy as np
+import pandas as pd
+
 import badbaby.python.return_dataframes as rd
 from badbaby.python import defaults
 
 # from score import score
 
 try:
-    # Use niprov as handler for events, or if it"s not installed, ignore
+    # Use niprov as handler for events, or if it's not installed, ignore
     from niprov.mnefunsupport import handler
 except ImportError:
     handler = None
-df = rd.return_dataframes("mmn")[0]
-ecg_chs = np.unique(df["ecg"].tolist())
-work_dir = defaults.paradigms["assr"]  # could be "mmn", "assr", "ids"
+df = rd.return_dataframes('mmn')[0]
+exclude = defaults.paradigms['exclusion']['mmn']
+df.drop(df[df.subjId.isin(pd.Series(exclude))].index, inplace=True)
+ecg_chs = np.unique(df['ecg'].tolist())
+work_dir = defaults.paradigms['mmn']  # could be 'mmn', 'assr', 'ids'
 
 for sr, decim in zip([1200, 1800], [2, 3]):
     for ch in ecg_chs:
         subjects = \
-            df[(df["samplingRate"] == sr) & (df["ecg"] == ch)][
-                "subjId"].tolist()
+            df[(df['samplingRate'] == sr) & (df['ecg'] == ch)][
+                'subjId'].tolist()
         if len(subjects) == 0:
             continue
         # noinspection PyTypeChecker
-        print("    \nUsing %d Hz as sampling rate and\n"
-              "    %s as ECG surrogate..." % (sr, ch))
-        print("    %d " % len(subjects), "Subjects: ", subjects)
+        print('    \nUsing %d Hz as sampling rate and\n'
+              '    %s as ECG surrogate...' % (sr, ch))
+        print('    %d ' % len(subjects), 'Subjects: ', subjects)
         params = mnefun.Params(tmin=-0.1, tmax=0.6, n_jobs=18,
-                               n_jobs_fir="cuda", n_jobs_resample="cuda",
-                               proj_sfreq=200, decim=decim,
-                               filter_length="30s", hp_cut=.1, hp_trans="auto",
-                               lp_cut=30., lp_trans="auto", bmin=-0.1,
+                               n_jobs_fir='cuda', n_jobs_resample='cuda',
+                               proj_sfreq=250, decim=decim,
+                               hp_cut=.1, hp_trans='auto',
+                               lp_cut=55., lp_trans='auto', bmin=-0.1,
                                ecg_channel=ch)
-        params.subjects = ["bad_%s" % ss for ss in subjects]
+        params.subjects = ['bad_%s' % ss for ss in subjects]
         # write prebad
         for si, subj in enumerate(subjects):
-            bad_channels = df[df["subjId"] == subj]["badCh"].tolist()
+            bad_channels = df[df['subjId'] == subj]['badCh'].tolist()
             if op.exists(op.join(work_dir, params.subjects[si],
-                                 "raw_fif")):
+                                 'raw_fif')):
                 prebad_file = op.join(work_dir, params.subjects[si],
-                                      "raw_fif",
-                                      "%s_prebad.txt" % params.subjects[si])
+                                      'raw_fif',
+                                      '%s_prebad.txt' % params.subjects[si])
                 if not op.exists(prebad_file):
-                    if bad_channels[0] == "None":
-                        with open(prebad_file, "w") as f:
-                            f.write("")
+                    if bad_channels[0] == 'None':
+                        with open(prebad_file, 'w') as f:
+                            f.write('')
                     else:
-                        bads = ["%s" % ch for ch in
-                                bad_channels[0].split(", ")]
-                        with open(prebad_file, "w") as output:
+                        bads = ['%s' % ch for ch in
+                                bad_channels[0].split(', ')]
+                        with open(prebad_file, 'w') as output:
                             for ch_name in bads:
-                                output.write("%s\n" % ch_name)
+                                output.write('%s\n' % ch_name)
         # Set the niprov handler to deal with events:
         params.on_process = None
         params.structurals = [None] * len(params.subjects)
@@ -70,42 +75,45 @@ for sr, decim in zip([1200, 1800], [2, 3]):
         params.dates = [None] * len(params.subjects)
         params.work_dir = work_dir
         params.subject_indices = np.arange(len(params.subjects))
-        
-        params.acq_ssh = "kambiz@minea.ilabs.uw.edu"  # minea
-        params.acq_dir = ["/data101/bad_baby", "/sinuhe/data01/bad_baby",
-                          "/sinuhe/data03/bad_baby", "/sinuhe_data01/bad_baby"]
-        params.sws_ssh = "kam@kasga.ilabs.uw.edu"  # kasga
-        params.sws_dir = "/data07/kam/bad_baby"
+
+        params.acq_ssh = 'kambiz@minea.ilabs.uw.edu'  # minea
+        params.acq_dir = ['/data101/bad_baby', '/sinuhe/data01/bad_baby',
+                          '/sinuhe/data03/bad_baby', '/sinuhe_data01/bad_baby']
+        params.sws_ssh = 'kam@kasga.ilabs.uw.edu'  # kasga
+        params.sws_dir = '/data07/kam/sss_work'
         # Set the parameters for head position estimation:
         params.coil_dist_limit = 0.01
-        params.coil_t_window = "auto"  # use the smallest reasonable window size
+        params.coil_t_window = 'auto'  # use the smallest reasonable window size
         # remove segments with < 3 good coils for at least 1 sec
         params.coil_bad_count_duration_limit = 1.  # sec
         # Annotation params
         params.rotation_limit = 20.  # deg/s
         params.translation_limit = 0.01  # m/s
         # Maxwell filter with mne-python
-        params.sss_type = "python"
-        params.sss_regularize = "in"
+        params.sss_type = 'python'
+        params.sss_regularize = 'in'
         params.tsss_dur = 4.
         params.int_order = 6
         params.st_correlation = .9
         params.trans_to = (0., 0., 0.06)
         # Covariance
         params.runs_empty = ['%s_erm_01']  # Define empty room runs
-        params.cov_method = 'shrunk'
+        params.cov_method = 'ledoit_wolf'
         params.compute_rank = True  # compute rank of the noise covar matrix
         params.force_erm_cov_rank_full = False  # compute and use the
         # empty-room rank
         # Trial/CH rejection criteria
         params.ssp_ecg_reject = dict(grad=np.inf, mag=np.inf)
         params.autoreject_thresholds = True
-        params.autoreject_types = ("mag", "grad")
+        params.autoreject_types = ('mag', 'grad')
         params.flat = dict(grad=1e-13, mag=1e-15)
+        params.auto_bad_reject = 'auto'
+        params.auto_bad_flat = params.flat
+        params.auto_bad_meg_thresh = 15
         # Proj
         params.get_projs_from = np.arange(1)
         params.proj_ave = True
-        params.proj_meg = 'combined'
+        # params.proj_meg = 'combined'
         params.inv_names = ['%s']
         params.inv_runs = [np.arange(1)]
         params.ecg_t_lims = (-0.04, 0.04)
@@ -113,27 +121,27 @@ for sr, decim in zip([1200, 1800], [2, 3]):
                             [0, 0, 0],  # EOG
                             [1, 1, 0]]  # Continuous (from ERM)
         # Inverse options
-        params.run_names = ["%s_am"]
+        params.run_names = ['%s_am']
         params.get_projs_from = np.arange(1)
-        params.inv_names = ["%s"]
+        params.inv_names = ['%s']
         params.inv_runs = [np.arange(1)]
         params.runs_empty = []
         # Conditioning
-        params.in_names = ["standard", "ba", "wa"]
+        params.in_names = ['standard', 'ba', 'wa']
         params.in_numbers = [103, 104, 105]
         params.analyses = [
-            "All",
-            "Individual",
-            "Oddball",
-            "Individual-matched",
-            "Oddball-matched"
+            'All',
+            'Individual',
+            'Oddball',
+            'Individual-matched',
+            'Oddball-matched'
         ]
         params.out_names = [
-            ["All"],
-            ["standard", "Ba", "Wa"],
-            ["standard", "deviant"],
-            ["standard", "Ba", "Wa"],
-            ["standard", "deviant"]
+            ['All'],
+            ['standard', 'Ba', 'Wa'],
+            ['standard', 'deviant'],
+            ['standard', 'Ba', 'Wa'],
+            ['standard', 'deviant']
         ]
         params.out_numbers = [
             [1, 1, 1],  # Combine all trials
@@ -149,19 +157,18 @@ for sr, decim in zip([1200, 1800], [2, 3]):
             [0, 1, 2],
             [0, 1]
         ]
-        default = False
-        cov = params.inv_names[0] + "-%.0f-sss-cov.fif" % params.lp_cut
+        cov = params.inv_names[0] + '-%.0f-sss-cov.fif' % params.lp_cut
         params.report_params.update(
             whitening=[
-                dict(analysis="All", name="All", cov=cov),
-                dict(analysis="Oddball-matched", name="standard", cov=cov),
-                dict(analysis="Oddball-matched", name="deviant", cov=cov)
+                dict(analysis='All', name='All', cov=cov),
+                dict(analysis='Oddball-matched', name='standard', cov=cov),
+                dict(analysis='Oddball-matched', name='deviant', cov=cov)
             ],
             sensor=[
-                dict(analysis="All", name="All", times="peaks"),
-                dict(analysis="Oddball-matched", name="standard",
-                     times="peaks"),
-                dict(analysis="Oddball-matched", name="deviant", times="peaks")
+                dict(analysis='All', name='All', times='peaks'),
+                dict(analysis='Oddball-matched', name='standard',
+                     times='peaks'),
+                dict(analysis='Oddball-matched', name='deviant', times='peaks')
             ],
             source=None,
             psd=True,
