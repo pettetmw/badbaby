34befca16213c70c736f16ed5e666d1a6ae2538c
Mark Pettet <pettetmw@gmail.com>
2019-07-08 14:44:47 -0700

    Use apply(include=...); Add comments; Clean up style

--- badbaby/python/sssraw2xdawnfilt.py
+++ badbaby/python/sssraw2xdawnfilt.py
@@ -9,21 +9,21 @@ Created on Fri Jun 28 07:27:29 2019
 @author: mpettet
 """
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 """Gist to fit XDAWN filter to ASSR response and viz ERF topography."""
 
 # based on xdawn_assr.py, except that it starts with raw_sss.fif
 # (rather than raw.fif), so we skip maxwell filter
 
-#__author__ = "Kambiz Tavabi"
-#__copyright__ = "Copyright 2019, Seattle, Washington"
-#__credits__ = ["Eric Larson"]
-#__license__ = "MIT"
-#__version__ = "1.0.1"
-#__maintainer__ = "Kambiz Tavabi"
-#__email__ = "ktavabi@uw.edu"
-#__status__ = "Production"
+# __author__ = "Kambiz Tavabi"
+# __copyright__ = "Copyright 2019, Seattle, Washington"
+# __credits__ = ["Eric Larson"]
+# __license__ = "MIT"
+# __version__ = "1.0.1"
+# __maintainer__ = "Kambiz Tavabi"
+# __email__ = "ktavabi@uw.edu"
+# __status__ = "Production"
 
 import mne
 import glob
@@ -32,74 +32,82 @@ import glob
 event_id = {'Auditory': 1}
 tmin, tmax = -0.5, 2.
 
-# ASSR
-print('Loading ASSR')
 
-def GetSsnData( aPFNm ):
+def GetSsnData(aPFNm):
+    # Given "a P(ath)F(ile) N(a)m(e)" to raw_sss.fif,
+    # plot/return XDAWN responses to signal and noise
     try:
-        raw = mne.io.read_raw_fif( aPFNm, allow_maxshield='yes' );
-        
+        raw = mne.io.read_raw_fif(aPFNm, allow_maxshield='yes')
+
         events = mne.find_events(raw)
         picks = mne.pick_types(raw.info, meg=True)
+        tNCh = picks.size  # number of channels
         epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
                             baseline=(None, 0), reject=dict(grad=4000e-13),
                             preload=True)
-        #epochs.average().plot()
-        
+        # epochs.average().plot()
+
         # signal_cov = mne.compute_covariance(epochs, tmin=0, tmax=1.5)
         # signal_cov = mne.cov.regularize(signal_cov, raw.info)
         signal_cov = mne.compute_covariance(epochs, method='oas', n_jobs=18)
         signal_cov = mne.cov.regularize(signal_cov, epochs.info, rank='full')
-        
+
         print('Fitting Xdawn')
-        xd = mne.preprocessing.Xdawn(n_components=1, signal_cov=signal_cov,
+        tNFC = 1  # number of xdawn filter components
+        xd = mne.preprocessing.Xdawn(n_components=tNFC, signal_cov=signal_cov,
                                      correct_overlap=False, reg='ledoit_wolf')
         xd.fit(epochs)
+
+        # fit() creates decomposition matrix called filters_ (or "unmixing")
+        # and the inverse, called "patterns_" for remixing responses after
+        # supressing contribution of selected filters_ components
+
+        # apply() method restricts the reponse to those projected on the
+        # filters_ components specified in the "include" arg.  The first tNFC
+        # components are the "signal" for purposes of SSNR optimization.
+
+        # calc "t(he) F(iltered) Evo(ked)" reponses
+        # (by default, include=list(arange(0,tNFC)), i.e., the "signal")
+        tFEvo = xd.apply(epochs)['Auditory'].average()
+
+        noiseinclude = list(arange(tNFC, tNCh))  # a range to include
+        # calc "t(he) Noi(se)"
+        tNoi = xd.apply(epochs, include=noiseinclude)['Auditory'].average()
         
-        #xd.apply(epochs)['Auditory'].average().plot_topo()
-        
-        tEI = epochs.info;
-        tEI['sfreq']=1; # we want each second to represent a different row of filter matrix
-        
-        tSig = np.dot( xd.filters_['Auditory'][0,:], epochs.average().data ).T;
-        tNoi = np.dot( xd.filters_['Auditory'][1:,:].mean(0), epochs.average().data ).T;
-        tSNTopo = np.array( [ xd.filters_['Auditory'][:,0].T, xd.filters_['Auditory'][:,1:].mean(1).T ] ).T;
-        
-#        # these two plots work, but better choice would be plot_joint()
-#        mne.EvokedArray( tSNTopo, tEI ).plot_topomap(times=[0,1]);
-#        figure(); plot( epochs.times, np.array([ tSig, tNoi ]).T ); legend( [ 'signal', 'noise' ] );
-        
+        # create arg to force both plots to have same fixed scaling
+        ts_args = dict(ylim=dict(grad=[-100, 100], mag=[-500, 500]))
+        tFEvo.plot_joint(ts_args=ts_args)
+        tNoi.plot_joint(ts_args=ts_args)
+
+#        # fit() also computes xd.evokeds_ which seems to be the same as
+#        # epochs.average(), but it's calculated in a complicated way that
+#        # compensates for overlap (when present).
+#        # Keep this handy to compare with xdawn results.
+#        xd.evokeds_['Auditory'].average().plot_joint(ts_args=ts_args)
+#        epochs.average().plot_joint(ts_args=ts_args)
+
     except:
-        tSig = None; tNoi = None; tSNTopo = None;
-    
-    return tSig, tNoi, tSNTopo
+        tFEvo = None
+        tNoi = None
+
+    return tFEvo, tNoi
 
 
-# this glob gets all a's and b's in bash, but not pythong: bad_*{a,b}/sss_fif/*_am_raw_sss.fif
-aPFNmPattern = '/media/ktavabi/ALAYA/data/ilabs/badbaby/tone/bad_*b/sss_fif/*_am_raw_sss.fif'
+# With this above function defined, we can use a glob expression to
+# to specify list of path-file name ("PFNm") strings
+
+# this glob gets all a's and b's in bash, but not python:
+#    bad_*{a,b}/sss_fif/*_am_raw_sss.fif
+
+#aPFNmPattern = '/media/ktavabi/ALAYA/data/ilabs/badbaby/tone/bad_*b/sss_fif/*_am_raw_sss.fif'
+
+aPFNmPattern = '/media/ktavabi/ALAYA/data/ilabs/badbaby/larson_eric_tone_raw_sss.fif'
 
 # Additional code needed here to check exclusion list in defaults.py
 
 # Start with second visits, labeled "B"
-tPFNmsB = glob.glob( aPFNmPattern );
-tBadB = [ GetSsnData( fp ) for fp in tPFNmsB ];
+tPFNmsB = glob.glob(aPFNmPattern)
 
-# Now repeat with first visits, "A"
-tPFNmsA = [ x.replace( 'b/sss', 'a/sss' ).replace( 'b_am', 'a_am' ) for x in tPFNmsB ];
-tBadA = [ GetSsnData( fp ) for fp in tPFNmsA ];
-
-tSigAs = [ x[0] for x in tBadA ]; 
-tNoiAs = [ x[1] for x in tBadA ];
-tSNTopoAs = [ x[2] for x in tBadA ];
-
-tSigBs = [ x[0] for x in tBadB ];
-tNoiBs = [ x[1] for x in tBadB ];
-tSNTopoBs = [ x[2] for x in tBadB ];
-
-
-
-
-
-
-
-
+tTest = GetSsnData(tPFNmsB[0])
+tFEvo = tTest[0]
+tNoi = tTest[1]
