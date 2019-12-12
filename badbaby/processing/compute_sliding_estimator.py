897ab2823d10414d7943b29a4976656d85b7916d
Kambiz Tavabi <ktavabi@uw.edu>
2019-12-11 18:36:45 -0800

    WIP GLM

--- badbaby/processing/compute_sliding_estimator.py
+++ badbaby/processing/compute_sliding_estimator.py
@@ -11,6 +11,7 @@ import os.path as op
 
 import matplotlib.pyplot as plt
 import numpy as np
+import xarray as xr
 from meeg_preprocessing import config
 from meeg_preprocessing.utils import combine_events
 from mne import read_epochs, EvokedArray, grand_average
@@ -18,7 +19,6 @@ from mne.decoding import (
     SlidingEstimator,
     cross_val_multiscore, LinearModel, get_coef
     )
-from mne.externals.h5io import write_hdf5
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import StratifiedKFold
@@ -29,105 +29,110 @@ import badbaby.return_dataframes as rd
 from badbaby import defaults
 
 workdir = defaults.datapath
+analysese = ['Individual', 'Oddball']
 plt.style.use('ggplot')
 ages = [2, 6]
 solver = 'liblinear'
-cl = 'auto'
 lp = defaults.lowpass
-combine = True
-if combine:
-    tag = '2v'
-    combos = list(itertools.combinations(['standard', 'deviant'], 2))
-else:
-    tag = '3v'
-    combos = list(itertools.combinations(defaults.oddball_stimuli, 2))
 window = defaults.peak_window
-n_splits = 7  # how many folds to use for cross-validation
+n_splits = 10  # how many folds to use for cross-validation
 le = LabelEncoder()
-for aix in ages:
-    scores = {kk: list() for kk in combos}
-    auc = {kk: list() for kk in combos}
-    evokeds = {kk: list() for kk in combos}
-    patterns = {kk: list() for kk in combos}
-    rstate = np.random.RandomState(42)
-    df = rd.return_dataframes('mmn', age=aix)[0]
-    subjects = ['bad_%s' % ss for ss in df.index]
+seed = np.random.RandomState(42)
+joint_kwargs = dict(ts_args=dict(gfp=True, time_unit='s'),
+                    topomap_args=dict(sensors=False, time_unit='s'))
 
-    hf_fname = op.join(defaults.datadir,
-                       '%smos_%d_%s_%s-cv-scores.h5' % (aix, lp, solver, tag))
-    for cii, cs in enumerate(combos):
-        for si, subject in enumerate(subjects):
-            ep_fname = op.join(workdir, subject, 'epochs',
-                               'All_%d-sss_%s-epo.fif' % (lp, subject))
-            epochs = read_epochs(ep_fname)
-            epochs.apply_baseline()
-            epochs.drop_bad()
-            if combine:
-                # Combine across deviant trials
-                epochs = combine_events(epochs, ['ba', 'wa'], {'deviant': 23})
-            epochs.equalize_event_counts(epochs.event_id.keys())
-            info = epochs.info
-            time = epochs.times
-            ix = epochs.time_as_index(window[0])[0], \
-                 epochs.time_as_index(window[1])[0]
-            condition1, condition2 = list(epochs[cs].event_id.keys())
-            clf = make_pipeline(RobustScaler(),
-                                LogisticRegression(solver='liblinear',
-                                                   penalty='l2',
-                                                   max_iter=1000,
-                                                   multi_class='auto',
-                                                   random_state=rstate))
-            time_decode = SlidingEstimator(clf, n_jobs=config.N_JOBS,
-                                           scoring='roc_auc',
-                                           verbose=True)
-            # K-fold cross-validation with ROC area under curve score
-            cv = StratifiedKFold(n_splits=n_splits, shuffle=True,
-                                 random_state=rstate)
-            # Get the data and label
-            X = epochs[cs].get_data()
-            y = le.fit_transform(epochs[cs].events[:, -1])
-            # logit AUC b/c chance level same regardless of the class balance
-            scores[cs].append(
-                np.mean(cross_val_multiscore(time_decode, X=X, y=y,
-                                             cv=cv,
-                                             n_jobs=config.N_JOBS),
-                        axis=0))
-            # Average AUC score across MMN window samples
-            auc[cs].append(scores[cs][-1][ix[0]:ix[1]].mean())
-            print("Subject %s : %s vs. %s mean AUC: %.3f" % (
-                subject, condition1, condition2, auc[cs][-1].mean()))
-            clf = make_pipeline(StandardScaler(),
-                                LinearModel(
-                                    LogisticRegression(solver='liblinear',
+for iii, analysis in enumerate(analysese):
+    if iii == 0:
+        conditions = ['standard', 'ba', 'wa']
+        combine = False
+    else:
+        conditions = ['standard', 'deviant']
+        combine = True
+    combos = list(itertools.combinations(conditions, 2))
+    for aix in ages:
+        scores = {kk: list() for kk in combos}
+        auc = {kk: list() for kk in combos}
+        evokeds = {kk: list() for kk in combos}
+        patterns = {kk: list() for kk in combos}
+        df = rd.return_dataframes('mmn')[0]
+        subjects = ['bad_%s' % ss for ss in df.index]
+        fi_out = op.join(defaults.datadir,
+                         '%smos_%d_%s_%s-slidingEstimator.nc'
+                         % (aix, lp, solver, analysis))
+        for cii, cs in enumerate(combos):
+            contrast = '_vs_'.join(cs)
+            print('Fitting estimator for %s: ' % contrast)
+            for si, subject in enumerate(subjects):
+                ep_fname = op.join(workdir, subject, 'epochs',
+                                   'All_%d-sss_%s-epo.fif' % (lp, subject))
+                epochs = read_epochs(ep_fname)
+                epochs.apply_baseline()
+                epochs.drop_bad()
+                if combine:
+                    # Combine across deviant trials
+                    epochs = combine_events(epochs, ['ba', 'wa'],
+                                            {'deviant': 23})
+                epochs.equalize_event_counts(epochs.event_id.keys())
+                info = epochs.info
+                time = epochs.times
+                ix = epochs.time_as_index(window[0])[0], \
+                     epochs.time_as_index(window[1])[0]
+                condition1, condition2 = list(epochs[cs].event_id.keys())
+                clf = make_pipeline(RobustScaler(),
+                                    LogisticRegression(solver=solver,
                                                        penalty='l2',
                                                        max_iter=1000,
                                                        multi_class='auto',
-                                                       random_state=rstate)))
-            time_decode = SlidingEstimator(clf, n_jobs=config.N_JOBS,
-                                           scoring=roc_auc_score,
-                                           verbose=True)
-            coef = get_coef(time_decode.fit(X, y), 'patterns_',
-                            inverse_transform=True)
-            # Get regression spatial patterns
-            evokeds[cs].append(EvokedArray(coef, epochs.info,
-                                           tmin=epochs.times[0]))
-        # Group averaged (ages) cv-score topomaps
-        joint_kwargs = dict(ts_args=dict(gfp=True, time_unit='s'),
-                            topomap_args=dict(sensors=False, time_unit='s'))
-        hs = grand_average(list(evokeds[cs])).plot_joint(
-            times=np.arange(window[0],
-                            window[1], .05),
-            title='patterns', **joint_kwargs)
-        this = '_vs_'.join(cs)
-        for hx, ch in zip(hs, ['mag', 'grad']):
-            hx.savefig(op.join(defaults.figsdir,
-                               '%dmos-avr-%s-%s-%s-topo_.png' %
-                               (aix, solver, this, ch)),
-                       bbox_inches='tight')
-    # Write age-cohort logit results to disk
-    write_hdf5(hf_fname,
-               dict(subjects=subjects,
-                    scores=list(scores.values()),
-                    auc=list(auc.values()),
-                    tvec=time),
-               title=solver, overwrite=True)
+                                                       random_state=seed))
+                time_decode = SlidingEstimator(clf, n_jobs=config.N_JOBS,
+                                               scoring='roc_auc',
+                                               verbose=True)
+                # K-fold cross-validation with ROC area under curve score
+                cv = StratifiedKFold(n_splits=n_splits, shuffle=True,
+                                     random_state=seed)
+                # Get the data and label
+                X = epochs[cs].get_data()
+                y = le.fit_transform(epochs[cs].events[:, -1])
+                # AUC b/c chance level same regardless of the class balance
+                scores[cs].append(
+                    np.mean(cross_val_multiscore(time_decode, X=X, y=y,
+                                                 cv=cv,
+                                                 n_jobs=config.N_JOBS), axis=0))
+                # Average AUC score across MMN window samples
+                auc[cs].append(scores[cs][-1][ix[0]:ix[1]].mean())
+                print("     Subject %s : %s vs. %s mean AUC: %.3f" % (
+                    subject, condition1, condition2, auc[cs][-1].mean()))
+                clf = make_pipeline(StandardScaler(),
+                                    LinearModel(
+                                        LogisticRegression(solver=solver,
+                                                           penalty='l2',
+                                                           max_iter=1000,
+                                                           multi_class='auto',
+                                                           random_state=seed)))
+                time_decode = SlidingEstimator(clf, n_jobs=config.N_JOBS,
+                                               scoring=roc_auc_score,
+                                               verbose=True)
+                coef = get_coef(time_decode.fit(X, y), 'patterns_',
+                                inverse_transform=True)
+                # Get regression spatial patterns
+                evokeds[cs].append(EvokedArray(coef, epochs.info,
+                                               tmin=epochs.times[0]))
+            # Group averaged (ages) cv-score topomaps
+            hs = grand_average(list(evokeds[cs])).plot_joint(
+                times=np.arange(window[0],
+                                window[1], .05),
+                title='patterns', **joint_kwargs)
+            for hx, ch in zip(hs, ['mag', 'grad']):
+                hx.savefig(op.join(defaults.figsdir,
+                                   '%dmos-avr-%s-%s-%s-topo_.png' %
+                                   (aix, solver, contrast, ch)),
+                           bbox_inches='tight')
+        
+        # Write logit results to disk
+        print('     ...writing %s: ' % op.basename(fi_out))
+        assert len(list(auc.values())) == len(combos)
+        foo = xr.DataArray(np.array((list(auc.values()))),
+                           coords=[['_vs_'.join(cc) for cc in combos],
+                                   subjects],
+                           dims=['contrast', 'subject'])
+        foo.to_netcdf(fi_out)
--- badbaby/processing/write_mmn_cdi_RMds.py
+++ badbaby/processing/write_mmn_cdi_RMds.py
@@ -6,42 +6,57 @@ tidy format.
 
 import os.path as op
 import re
-
-import numpy as np
 import pandas as pd
-from mne.externals.h5io import read_hdf5
+import seaborn as sns
+import xarray as xr
+import itertools
 
 import badbaby.return_dataframes as rd
 from badbaby import defaults
 
+# Parameters
 datadir = defaults.datadir
 analysese = ['Individual', 'Oddball']
-
 ages = [2, 6]
 lp = defaults.lowpass
 regex = r"[abc]$"
-tag = '2v'
 solver = 'liblinear'
 
+# Wrangle MEG & CDI covariates
 meg, cdi = rd.return_dataframes('mmn', ses=True)
-ids = [re.split(regex, ss)[0] for ss in meg.index]
 meg.reset_index(inplace=True)
-meg['subjId'] = ['BAD_%s' % xx for xx in ids]
-
-results = pd.merge(meg[['ses', 'age', 'gender', 'headSize', 'maternalEdu',
-                        'maternalHscore', 'paternalEdu', 'paternalHscore',
-                        'maternalEthno', 'paternalEthno', 'birthWeight',
-                        'subjId']], cdi, on='subjId', validate='m:m')
-dfs = list()
-for age in ages:
-    df_ = read_hdf5(op.join(datadir,
-                            '%dmos_%d_%s_%s-cv-scores.h5' % (age, lp,
-                                                             solver, tag)),
-                    title=solver)
-    df_['ag'] = np.ones((len(df_))) * age
-    dfs.append(pd.DataFrame.from_dict(df_))
-df = pd.concat([pd.DataFrame(dfs[0]),
-                pd.DataFrame(dfs[1])], axis=1, sort=True)
-df['subjId'] = [re.split(regex, ss)[0].upper() for ss in df.index.values]
-results = pd.merge(df, results, on='subjId', validate='m:m')
-results.to_csv(op.join(datadir, 'mmn-%d_cdi_RMds.csv' % lp))
+meg['subject'] = ['bad_%s' % xx for xx in meg.subjId]
+meg['subjId'] = [re.split(regex, ss)[0].upper() for ss in meg.subjId]
+meg['subjId'] = ['BAD_%s' % xx for xx in meg.subjId]
+s_ = meg.age < 80
+meg['group'] = s_.map({True: '2mos', False: '6mos'})
+covars = pd.merge(meg[['simmInclude', 'ses', 'age', 'gender', 'headSize',
+                       'maternalEdu', 'maternalHscore',
+                       'paternalEdu', 'paternalHscore',
+                       'maternalEthno', 'paternalEthno', 'birthWeight',
+                       'subjId', 'subject', 'group']], cdi, on='subjId',
+                  validate='m:m')
+g = sns.catplot(x='group', y='age', height=6, aspect=2, kind='box', data=covars)
+# Wrangle MEG response vars
+for iii, analysis in enumerate(analysese):
+    print('Reading data for %s analysis... ' % analysis)
+    if iii == 0:
+        conditions = ['standard', 'ba', 'wa']
+    else:
+        conditions = ['standard', 'deviant']
+    combos = list(itertools.combinations(conditions, 2))
+    dfs = list()
+    for age in ages:
+        fi_in = op.join(defaults.datadir,
+                        '%smos_%d_%s_%s-slidingEstimator.nc'
+                        % (age, lp, solver, analysis))
+        print('    File in %s...' % op.basename(fi_in))
+        ds = xr.open_dataarray(fi_in)
+        dfs.append(ds.to_dataframe(name='AUC').reset_index())
+    # ROC Area under curve
+    # df = pd.concat(dfs, axis=1, verify_integrity=True, ignore_index = True)
+    df_ = dfs[0].merge(dfs[1], on=['contrast', 'subject'],
+                       suffixes=('_2mos', '_6mos'), validate='m:m')
+    df = df_.merge(covars, on='subject', validate='m:m')
+    df.to_csv(op.join(datadir, 'auc_%d_%s_%s-RM.csv' % (lp, solver,
+                                                        analysis)))
