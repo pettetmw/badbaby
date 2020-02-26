ba5d1918956c84afd28e4270e137e3b624398dcc
Kambiz Tavabi <ktavabi@gmail.com>
2020-02-25 19:33:33 -0800

    JAMOVI
    ANOVA and Regression modeling

--- badbaby/viz/plot_sliding_estimator.py
+++ badbaby/viz/plot_sliding_estimator.py
@@ -6,11 +6,9 @@ ERFs."""
 import itertools
 import os.path as op
 
-import numpy as np
 import pandas as pd
 import seaborn as sns
 import xarray as xr
-from scipy import stats
 import badbaby.return_dataframes as rd
 from badbaby import defaults
 
@@ -24,12 +22,13 @@ window = defaults.peak_window
 groups = ['2mos', '6mos']
 solver = 'lbfgs'
 regex = r"[abcd_]$"
+idx = pd.IndexSlice
 
 # Wrangle df1 & df2 covariates
 df1, df2 = rd.return_dataframes('mmn')
 df1.reset_index(inplace=True)
 ma_ = df1.age < 80
-rm_ids = df1[df1.simmInclude == 1].subjId
+auc_ds_ids = df1[df1.simmInclude == 1].subjId
 df1['group'] = ma_.map({True: '2mos', False: '6mos'})
 df2['subjId'] = [xx.lstrip('BAD_') for xx in df2.subjId]
 df1['dummy'] = [xx.rstrip(regex) for xx in df1.subjId]
@@ -41,7 +40,6 @@ covars = covars[['simmInclude', 'ses', 'age', 'gender',
                  'paternalEdu', 'paternalHscore',
                  'maternalEthno', 'paternalEthno', 'birthWeight',
                  'group', 'cdiAge', 'm3l', 'vocab']]
-
 for iii, analysis in enumerate(analysese):
     if iii == 0:
         conditions = ['standard', 'ba', 'wa']
@@ -71,11 +69,15 @@ for iii, analysis in enumerate(analysese):
                  % (lp, solver, analysis))
     aucs = xr.open_dataarray(fi).to_dataframe('auc')
     aucs.reset_index(inplace=True, level=[0, 1])
-    aucs = aucs.merge(covars, left_on='subject', right_on='subjId', how='outer')
+    aucs_covars = aucs.merge(covars, left_on='subject', right_on='subjId',
+                             how='outer')
+    aucs_covars.to_csv(op.join(defaults.datadir, 'AUC_%s_%d_%s.csv' %
+                               (analysis, lp, solver)))
     g = sns.catplot(x='subjId', y='auc', hue='group', row='contrasts',
-                    data=aucs[aucs.cdiAge == 18], kind='bar', ci=None,
+                    data=aucs_covars[aucs_covars.cdiAge == 18], kind='bar',
+                    ci=None,
                     height=3,
-                    aspect=6, dodge=False, legend=False)
+                    aspect=6, dodge=False, legend=True)
     ax = g.fig.gca()
     ax.set_xticklabels(ax.xaxis.get_majorticklabels(),
                        rotation=90, fontweight='light',
@@ -86,7 +88,7 @@ for iii, analysis in enumerate(analysese):
     ax.legend()
     g.savefig(op.join(defaults.figsdir, 'IND-SCORES_%d_%s.png' % (lp, solver)))
     g = sns.catplot(x='group', y='auc',
-                    data=aucs[aucs.cdiAge == 18], kind='boxen',
+                    data=aucs_covars[aucs_covars.cdiAge == 18], kind='boxen',
                     order=groups, row='contrasts', row_order=contrasts,
                     height=6, legend=False)
     ax = g.fig.gca()
@@ -95,20 +97,3 @@ for iii, analysis in enumerate(analysese):
     ax.set_xlabel('Age')
     ax.legend()
     g.savefig(op.join(defaults.figsdir, 'GRP-SCORES_%d_%s.png' % (lp, solver)))
-    rm = aucs[aucs.simmInclude == 1]
-    rm = rm.pivot_table(index=['group', 'subject'])
-    pieces = dict(zip(groups, [rm.loc[ag, :] for ag in groups]))
-    for kk, vv in pieces.items():
-        vv.insert(0, 'ids', np.unique([xx.strip(regex) for xx in vv.index]))
-        pieces[kk].update((kk, vv.reset_index(drop=True)))
-    result = pd.merge(pieces['2mos'][['ids', 'age', 'auc']], pieces['6mos'],
-                      on='ids',
-                      suffixes=groups)
-    result.to_csv(op.join(defaults.datadir, 'RM-SCORES_%d_%s.csv' % (lp,
-                                                                     solver)))
-    for cc in contrasts:
-        x = result.auc2mos.values
-        y = result.auc6mos.values
-        # Wilcoxon signed-rank test Alt H0 2- < 6-months
-        stat, pval = stats.wilcoxon(x, y, alternative="less")
-        print('%s (W, P-value): (%f, %f)' % (cc, stat, pval))
