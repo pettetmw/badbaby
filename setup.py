d80a2db7c90289715accdc9ad6e91e1919a6a62c
Kambiz Tavabi <ktavabi@gmail.com>
2020-12-17 13:23:07 -0800

    MAINT scripts to notebook(s)

--- setup.py
+++ setup.py
@@ -10,28 +10,27 @@ DESCRIPTION = 'speech infants MEG'
 URL = 'https://github.com/ktavabi/badbaby'
 EMAIL = 'ktavabi@gmail.com'
 AUTHOR = 'Kambiz Tavabi'
-VERSION = “0”
-REQUIRED = ['janitor',
-'']
+VERSION = '0.1'
+REQUIRED = ['janitor', 'pandas-profiling']
 
-here = os.path.abspath(os.path.dirname(__file__))# Where the magic happens:
+here = os.path.abspath(os.path.dirname(__file__))  # Where the magic happens:
 setup(
-        name=NAME,
-        description=DESCRIPTION,
-        author=AUTHOR,
-        author_email=EMAIL,
-        version=VERSION,
-        url=URL,
-        packages=find_packages(exclude=('tests',)),
-        install_requires=REQUIRED,
-        include_package_data=True,
-        license='MIT',
-        classifiers=[
-                # Trove classifiers
-                # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-                'License :: OSI Approved :: MIT License',
-                'Programming Language :: Python :: 3.6',
-                'Intended Audience :: Science/Research',
-                'Topic :: Scientific/Engineering'
-                ]
-                )
+    name=NAME,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    version=VERSION,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering'
+    ]
+)
