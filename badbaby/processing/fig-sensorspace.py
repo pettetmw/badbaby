58ea300b4f8ffe35859c3351067af806b38539ee
Kambiz Tavabi <ktavabi@gmail.com>
2020-12-17 22:55:42 -0800

    Revert "MAINT refactor on inclusion picks"
    This reverts commit 773467124c3183ab57c35fcf87640d0da778596c.

--- badbaby/processing/fig-sensorspace.py
+++ badbaby/processing/fig-sensorspace.py
@@ -1,45 +1,43 @@
 # %%
+import os
 from os import path as op
 import matplotlib.pyplot as plt
 import mne
 from mne.utils.numerics import grand_average
 from badbaby import defaults
 
-plt.style.use("ggplot")
+# %%
+plt.style.use('ggplot')
 workdir = defaults.datadir
 tmin, tmax = defaults.epoching
 lp = defaults.lowpass
 window = defaults.peak_window  # peak ERF latency window
 
 # %%
-evokeds = {"standard": [], "deviant": []}
+df = defaults.return_dataframes('mmn')[0]  # six monthers
+subjects = ['bad_%s' % ss for ss in df[df['age'] > 150].index]  # sixers
+evokeds = {'standard': [], 'deviant': []}
 for condition in evokeds.keys():
-    for si, subj in enumerate(defaults.cohort_six):
-        print(" %s" % subj)
-        filename = op.join(
-            defaults.datadir,
-            subj,
-            "inverse",
-            "Oddball_%d-sss_eq_%s-ave.fif" % (lp, subj),
-        )
-        erf = mne.read_evokeds(
-            filename,
-            condition=condition,
-            baseline=(None, 0)
-        )
+    for si, subj in enumerate(subjects):
+        print(' %s' % subj)
+        filename = op.join(defaults.datadir, subj, 'inverse','Oddball_%d-sss_eq_%s-ave.fif' % (lp, subj))
+        erf = mne.read_evokeds(filename, condition=condition,
+                       baseline=(None, 0))
+        if erf.info['sfreq'] > 600.0:
+            raise ValueError('Wrong sampling rate!')
+        if len(erf.info['bads']) > 0:
+            erf.interpolate_bads()
         evokeds[condition].append(erf)
+print(evokeds)
 
 # %%
 ERFS = dict()
 for kk in evokeds.keys():
-    ERFS[kk] = grand_average(evokeds[kk])
-peak = ERFS["deviant"].get_peak(
-                                ch_type="grad",
-                                tmin=window[0],
-                                tmax=window[1]
-                                )
-mne.viz.plot_compare_evokeds(ERFS, combine="gfp", ci=0.9)
+    ERFS[kk]=mne.grand_average(evokeds[kk])
+peak = ERFS['deviant'].get_peak(ch_type='grad', tmin=window[0], 
+                                tmax=window[1])
+mne.viz.plot_compare_evokeds(ERFS, combine='gfp', ci=0.9)
 
-# TODO
+#TODO 
 # [] group level MMN interval windowing around deviant peak latency
-# [] random individual (3) subjects topoplots w/ focus on peak deviant ERF
+# [] individual subject data for ERF conditions
